4cd27bc59b289b37e8fbd4d83e406cb44b281a53
Duane E Burnett <info@guighost.com>
2020-02-02 21:50:21 -0500

    more enhance

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-enhance/www/enhance.js",
+        "id": "cordova-plugin-enhance.Enhance",
+        "pluginId": "cordova-plugin-enhance",
+        "clobbers": [
+            "Enhance"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-enhance": "3.0.0"
+}
+// BOTTOM OF METADATA
+});
